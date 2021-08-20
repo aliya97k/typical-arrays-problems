cd3a2608fe29a3e9edaf91f76d863ed5cfc9e1a2
aliya97k <aliyakussainova13@gmail.com>
2021-08-20 20:37:27 +0600

    feat: find min max avg number

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 exports.min = function min (array) {
-  if (!array || array.length === 0) {
+  if (array === undefined || array.length === 0 ) {
     return 0;
 }
-  return Math.min.apply(null, array);
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  if (!array || array.length === 0) {
+  if (array === undefined || array.length === 0 ) {
     return 0;
 } else {
-  return Math.max.apply(null, array);
+  return Math.max(...array);
 }
 }
 
 exports.avg = function avg (array) {
-  if (!array || array.length === 0) {
+  if (array === undefined || array.length === 0 ) {
     return 0;
 } else {
-  let array = array.reduce(function(a, b) { return a + b; }, 0);
-  return array / array.length;
+  return array.reduce((a, b) => (a + b)) / array.length;
 }
 } 
